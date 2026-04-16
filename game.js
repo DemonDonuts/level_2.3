13452c1b48370e3538fc82ba5dc366b47a427f9e
DemonDonuts <safeliz928@gmail.com>
2026-04-16 13:00:59 -0400

    previous

---
+++ game.js
@@ -0,0 +1,45 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var player1 = new GameObject();
+var keys = {};
+
+console.log(document.getElementById("canvas"));
+
+player1.x = 20;
+player1.y = canvas.height / 2 - 50;
+player1.width = 20;
+player1.height = 100;
+player1.color = "blue";
+player1.speed = 3;
+
+// makes sure that when player presses a key, the system knows (dyslexia)
+document.addEventListener("keydown", function(e) 
+{
+    keys[e.key] = true;
+});
+
+document.addEventListener("keyup", function(e) 
+{
+    keys[e.key] = false;
+});
+
+
+setInterval(animate, 1000 / 60);
+
+function animate() 
+{
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    //keys up and down
+    if (keys["w"] && player1.y > 0)
+    {
+        player1.y -= player1.speed;
+    }
+
+    if (keys["s"] && player1.y + player1.height < canvas.height)
+    {
+        player1.y += player1.speed;
+    }
+
+    player1.drawRect(context);
+}
